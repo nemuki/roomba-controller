f9f5fceb47d7e391e27f81d7d15d477343ea2fc5
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-07-19 11:13:12 +0900

    debug.pyの追加

---
+++ debug.py
@@ -0,0 +1,11 @@
+import back_home
+import darth_bader
+import go_straight
+import turn_left
+import turn_right
+import time
+
+go_straight.main(2)
+turn_left.main(180)
+go_straight.main(2)
+turn_right.main(180)
